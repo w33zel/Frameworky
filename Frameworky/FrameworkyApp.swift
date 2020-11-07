235cdbb64edf4242f3e403fa4b269c19ff2e7b52
w33zel <fu.please@gmail.com>
2020-11-07 15:53:58 +0100

    Initial Commit

---
+++ Frameworky/FrameworkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworkyApp.swift
+//  Frameworky
+//
+//  Created by Christian Arlt on 07.11.20.
+//
+
+import SwiftUI
+
+@main
+struct FrameworkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
